9d5d42a89eed3c1f356daca5d453ea8b5b1c9eec
Ted Chen <nanflasted@gmail.com>
2019-08-02 22:51:27 -0700

    Hacj master (#18)
    * Storytime now uses pure python
    Added more stories to corpus
    Fixed formatting of user names (wrapped in angle brackets now)
    Fixed globs

--- tekinbot/comms/message/storytime/storytime.py
+++ tekinbot/comms/message/storytime/storytime.py
@@ -1,16 +1,48 @@
-
+import glob
 import random
 import re
-import time
-import os
+
+import markovify
 
 import tekinbot.utils.post as pu
 from tekinbot.utils.config import tekin_id
 
-comm_re = re.compile(f'{tekin_id} tell me a story', flags=re.IGNORECASE)
+comm_re = re.compile(f'{tekin_id}.*tell.*a story', flags=re.IGNORECASE)
+ALL_CHARS = ["@nomworthy", "@jhoak", "@logan_w",
+             "@nanflasted", "@nomworthy", "@TekinBot"]
+
 
 def process(request):
-    return os.system("ruby generate.rb")
+    # Choose 2..5 stories.
+    stories = random.sample(
+        glob.glob("**/corpus/*", recursive=True),
+        random.randint(2, 5)
+    )
+
+    models = []
+    # Create markov models and merge them.
+    for story in stories:
+        with open(story) as f:
+            models.append(markovify.Text(f.read(), well_formed=False))
+
+    finished_model = markovify.combine(models, range(len(stories)))
+
+    # Decide on paragraphs and sentence count.
+    para_length = []
+    for i in range(random.randint(2, 5)):
+        para_length.append(random.randint(3, 5))
+
+    story_text = "\n".join([
+        " ".join([
+            finished_model.make_sentence() for _ in range(para)
+        ]) for para in para_length
+    ])
+
+    characters = random.sample(ALL_CHARS, 3)
+    for (token, character) in zip(["_A_", "_B_", "_C_"], characters):
+        story_text = story_text.replace(token, character)
+
+    return story_text
 
 
 def post(request, response):

ff7858ec84a2afc6459ae31e69447391b7c0fd46
Ted Chen <nanflasted@gmail.com>
2019-08-03 05:49:40 +0000

    fix globs

--- tekinbot/comms/message/storytime/storytime.py
+++ tekinbot/comms/message/storytime/storytime.py
@@ -1,46 +1,49 @@
 import glob
-import markovify
 import random
 import re
-import time
-import os
+
+import markovify
 
 import tekinbot.utils.post as pu
 from tekinbot.utils.config import tekin_id
 
 comm_re = re.compile(f'{tekin_id}.*tell.*a story', flags=re.IGNORECASE)
+ALL_CHARS = ["@nomworthy", "@jhoak", "@logan_w",
+             "@nanflasted", "@nomworthy", "@TekinBot"]
+
 
 def process(request):
     # Choose 2..5 stories.
-    stories = random.sample(glob.glob("corpus/*"),random.randint(2, 5)) 
+    stories = random.sample(
+        glob.glob("**/corpus/*", recursive=True),
+        random.randint(2, 5)
+    )
 
     models = []
     # Create markov models and merge them.
     for story in stories:
         with open(story) as f:
-            models.append(markovify.Text(f.read(),well_formed = False))
+            models.append(markovify.Text(f.read(), well_formed=False))
 
     finished_model = markovify.combine(models, range(len(stories)))
 
     # Decide on paragraphs and sentence count.
     para_length = []
     for i in range(random.randint(2, 5)):
-        para_length.append(random.randint(3,5))
+        para_length.append(random.randint(3, 5))
 
-    story = ""
-    for para in para_length:
-        for i in range(para):
-            sent = finished_model.make_sentence()
-            if(sent != None):
-                story += sent
-                story += " "
-        story += "\r\n"
+    story_text = "\n".join([
+        " ".join([
+            finished_model.make_sentence() for _ in range(para)
+        ]) for para in para_length
+    ])
 
-    characters = random.sample(["@nomworthy","@jhoak","@logan_w","@nanflasted","@nomworthy","@TekinBot"],3)
-    story = story.replace("_A_","<"+characters[0]+">")
-    story = story.replace("_B_","<"+characters[1]+">")
-    story = story.replace("_C_","<"+characters[2]+">")
-    return story
+    characters = random.sample(ALL_CHARS, 3)
+    for (token, character) in zip(["_A_", "_B_", "_C_"], characters):
+        story_text = story_text.replace(token, character)
+
+    return story_text
+
 
 def post(request, response):
     return pu.post_plain_text(request, response, auth=pu.bot_auth())

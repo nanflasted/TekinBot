e229979f1cabd6598aa5cd4245f70143d4be7f63
Mickey McCargish <mmccargishm@gmail.com>
2019-10-28 07:51:38 -0400

    Thanks Ted for suggestions

--- tekinbot/comms/message/choose.py
+++ tekinbot/comms/message/choose.py
@@ -10,17 +10,23 @@ comm_re = re.compile(f'^{tekin_id} choose (?P<message>.*)$', flags=re.IGNORECASE
 def process(request):
     match = re.fullmatch(comm_re, request['event']['text'])
     message = match.group('message')
-    choices = message.split(",")
-    num_ch = len(choices)
 
-    if num_ch > 0:
-      chosen = choices[random.randInt(num_ch)].strip()
+    if message:
+      choices = message.split(",")
+      num_ch = len(choices)
+      # Choose an option randomly
+      chosen = random.choice(choices).strip()
+      # Remove or if needed
       if chosen[:3] == "or ":
         chosen = chosen[3:].strip()
-      return f'I am {100/num_ch + max([random.randint(0, 100-(100/num_ch)) for i in range(num_ch)])}% sure you should choose {chosen}.'
+      # Come up with a valid chance
+      chance = 100/num_ch + max([
+          random.randint(0, 100-(100/num_ch)) for _ in range(num_ch)
+        ])
+      return f'I am {chance}% sure you should choose {chosen}.'
 
     else:
-      return "There is nothing to choose"
+      return "There is nothing to choose."
 
 
 def post(request, resp):
